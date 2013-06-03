4cc5821a19363a19125903a50c0b212d154dd2eb
Hussain Miah <hmiah.1993@gmail.com>
2013-06-03 20:02:11 +0100

    Added the ChatMessage class

---
+++ src/Hussain/ChatMessage.java
@@ -0,0 +1,34 @@
+package Hussain;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hussain
+ * Date: 03/06/2013
+ * Time: 19:55
+ * Project Name: JChat
+ */
+public class ChatMessage implements Serializable {
+    protected static final long serialVersionUID = 1112122200L;
+
+    protected static final int WHOIS = 0;
+    protected static final int MESSAGE = 1;
+    protected static final int LOGOUT = 2;
+
+    private int type;
+    private String message;
+
+    public ChatMessage(int type, String message){
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
---
+++ src/Hussain/Server.java
@@ -0,0 +1,11 @@
+package Hussain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Hussain
+ * Date: 03/06/2013
+ * Time: 19:57
+ * Project Name: JChat
+ */
+public class Server {
+}
