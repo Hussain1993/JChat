18f37463ee0e0a024dbf1d01714e826d30f809c1
Hussain Miah <hmiah.1993@gmail.com>
2013-06-04 14:39:52 +0100

    Added the constructor for the class

--- src/Hussain/Client.java
+++ src/Hussain/Client.java
@@ -1,5 +1,10 @@
 package Hussain;
 
+import GUI.ClientGUI;
+
+import java.io.*;
+import java.net.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Hussain
@@ -8,4 +13,26 @@ package Hussain;
  * Project Name: JChat
  */
 public class Client {
+    //This is for I/O
+    private ObjectInputStream inputStream;
+    private ObjectOutputStream outputStream;
+    private Socket socket;
+
+    private ClientGUI clientGUI; //If we are using the GUI or not
+
+    private String server; //This is the server
+    private String username; //This is the username of the person
+    private int port; //This is the port number
+
+    public Client(String server, String username, int port, ClientGUI clientGUI){
+        this.server = server;
+        this.username = username;
+        this.port = port;
+        this.clientGUI = clientGUI;
+    }
+
+    public Client(String server, String username, int port){
+        this(server,username,port,null);
+    }
+
 }
